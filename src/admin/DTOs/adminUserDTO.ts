fe21d586acccd45619ddb5d7662135d2780585ab
AzaKamala <azazuher@gmail.com>
2025-03-16 04:07:53 +0300

    Add admin user DTO

---
+++ src/admin/DTOs/adminUserDTO.ts
@@ -0,0 +1,39 @@
+class AdminUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(
+    id: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    role: string,
+    createdAt: Date,
+    updatedAt: Date
+  ) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.role = role;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  static fromObject(obj: any): AdminUser {
+    return new AdminUser(
+      obj.id,
+      obj.firstName,
+      obj.lastName,
+      obj.email,
+      obj.role,
+      obj.createdAt,
+      obj.updatedAt
+    );
+  }
+}
