84d473e0a47a07155df8485c037ae07f7bd36f47
AzaKamala <azazuher@gmail.com>
2025-03-16 04:18:54 +0300

    Add queries for admin user router

---
+++ src/admin/queries/adminUserQueries.ts
@@ -0,0 +1,98 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+export const createAdminUser = async (email: string, password: string, firstName: string, lastName: string, role: string) => {
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newAdminUser = await prisma.adminUser.create({
+            data: {
+                email,
+                password: hashedPassword,
+                firstName,
+                lastName,
+                role
+            },
+        });
+
+        return newAdminUser;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getAdminUsers = async () => {
+    try {
+        const adminUsers = await prisma.adminUser.findMany();
+
+        return adminUsers;
+    } catch(error) {
+        throw error;
+    }
+}
+
+export const getAdminUserById = async (id: string) => {
+    try {
+        const adminUser = await prisma.adminUser.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        return adminUser;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const getAdminUserByEmail = async (email: string) => {
+    try {
+        const adminUser = await prisma.adminUser.findUnique({
+            where: {
+                email,
+            },
+        });
+
+        return adminUser;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const updateAdminUser = async (id: string, email?: string, password?: string, firstName?: string, lastName?: string, role?: string) => {
+    try {
+        const data: any = {};
+
+        if (firstName) data.firstName = firstName;
+        if (lastName) data.lastName = lastName;
+        if (email) data.email = email;
+        if (role) data.role = role;
+        if (password) {
+            data.password = await bcrypt.hash(password, 10);
+        }
+
+        const updatedAdminUser = await prisma.adminUser.update({
+            where: {
+                id,
+            },
+            data,
+        });
+
+        return updatedAdminUser;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const deleteAdminUser = async (id: string) => {
+    try {
+        await prisma.adminUser.delete({
+            where: {
+                id,
+            },
+        });
+    } catch (error) {
+        throw error;
+    }
+}
