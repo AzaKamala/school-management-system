3383dea5053c0a5dba973473fb0256a82436068a
AzaKamala <azazuher@gmail.com>
2025-03-17 13:49:47 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import express from "express";
 import adminUserRouter from "./admin/routers/adminUserRouter";
-import tenantRouter from './admin/routers/tenantRouter';
-import userRouter from './tenant/routers/userRouter';
+import tenantRouter from "./admin/routers/tenantRouter";
+import userRouter from "./tenant/routers/userRouter";
+import authRouter from "./auth/routes/authRouter";
+import oauthRouter from "./auth/routes/oauthRouter";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/admin-user', adminUserRouter);
-app.use('/tenant', tenantRouter);
-app.use('/tenant/:tenantId/user', userRouter);
+app.use("/admin-user", adminUserRouter);
+app.use("/tenant", tenantRouter);
+app.use("/tenant/:tenantId/user", userRouter);
+app.use("/auth", authRouter);
+app.use("/auth/oauth", oauthRouter);
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
